3abffd02b64c081e7a11e0cd55ea411e90611b08
Abdullah <muhammadabdullahgohar572@gmail.com>
2024-02-29 01:18:53 -0800

    first commit

---
+++ src/assets/component/Contact/Contact.jsx
@@ -0,0 +1,417 @@
+import html from "../../img/Skill/5847f5bdcef1014c0b5e489c.webp";
+import css from "../../img/Skill/css-logo.webp";
+import tail from "../../img/Skill/Git/Tailwind_CSS_Logo.svg (convert.io).webp";
+import js from "../../img/Skill/javascript-seeklogo.com.webp";
+import React from "../../img/Skill/React.webp";
+import Nodejs from "../../img/Skill/nodejs-1-logo-svg-vector.webp";
+import Express from "../../img/Skill/express-js.webp";
+import MongoDb from "../../img/Skill/Git/Mongodb-PNG-Image-HD (convert.io).webp";
+import Mongoose from "../../img/Skill/mongoose.webp";
+import Git from "../../img/Skill/Git/Git-Icon-1788C (convert.io).webp";
+import Github from "../../img/Skill/Git/25231 (convert.io).webp";
+import Netlify from "../../img/Skill/Git/netlify-logo-svg-vector (convert.io).webp";
+import boost from '../../img/center/Bootstrap_logo.svg.png'
+import c from '../../img/center/C_Logo.png'
+import cc from '../../img/center/Csharp_Logo.png'
+import java from '../../img/center/java-logo-vector-1.svg'
+import posman from '../../img/center/postman-logo-0087CA0D15-seeklogo.com.png'
+import  redux from '../../img/center/redux-logo-vector.svg'
+
+import versal from '../../img/center/180x180.png'
+import next from '../../img/center/image_processing20210620-5261-1izw9zf.png'
+import fire from '../../img/center/mascot_firebase-logo.png'
+import  phpto from '../../img/center/pngimg.com - photoshop_PNG11.png'
+import  illus from '../../img/center/adobe-illustrator-circle-icon-for-web-design-free-png.webp'
+import  navtive from '../../img/center/react-native.png'
+
+
+
+
+
+
+import andhi from '../../img/chat/andhi.png'
+import chat from '../../img/chat/chatgpt-logo-transparent-background-free-png.webp'
+import  gamma from '../../img/chat/gamma.png'
+import mic from '../../img/chat/microsoft-designer.png'
+import stack   from '../../img/chat/stavk.png'
+import  chatt from '../../img/chat/sup.png';
+import pp from "../../img/chat/102047422.png"
+
+
+
+
+
+import one from '../../img/chat/chat1/download (4).png'
+import two from '../../img/chat/chat1/Font_Awesome_logomark_blue.svg.png'
+import  three from '../../img/chat/chat1/img0059.jpg'
+import four from '../../img/chat/chat1/Free-Premium-Mockups-Instant-Download-Mockups-Design-com.png'
+
+
+
+export const Contact = () => {
+  return (
+    <>
+      <header className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Contact Information</h1>
+      </header>
+
+      <main className="container mx-auto mt-8">
+        <section className="mb-8 sm:container sm:mx-auto sm:px-[5%]">
+          <h2 className="text-2xl font-semibold mb-4 sm:m-0 mx-[5%]">
+            Personal Information
+          </h2>
+          <div className="p-4 bg-white shadow-md">
+            <p>
+              <strong>Name:</strong> Muhammad Abdullah Gohar
+            </p>
+            <p>
+              <strong>Father's Name:</strong> Safdar Hussain
+            </p>
+            <p>
+              <strong>Phone No:</strong> 03133038410
+            </p>
+            <p>
+              <strong>Email:</strong> muhammadabdullahgohar572@gmail.com
+            </p>
+            <p>
+              <strong>Website:</strong>{" "}
+              <a href="https://mabdullahm10.netlify.app/" target="_blank">
+                mabdullahm14.netlify.app
+              </a>
+            </p>
+            <p>
+              <strong>Occupation:</strong> Full Stack Developer
+            </p>
+          </div>
+        </section>
+
+        <div className="container mx-auto text-center text-2xl">
+          <p>
+            <strong>Skills</strong>{" "}
+          </p>
+          <br />
+        </div>
+
+        <section className="mb-8 sm:ml-0 ml-[10%] sm:container text-center justify-center  sm:grid sm:grid-cols-6 sm:mx-auto sm:px-[5%] ">
+          <div>
+            <img
+              src={html}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+
+          <div>
+            <img
+              src={css}
+              alt=""
+              className="sm:w-[49%]  w-[40%]  sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:m-0 ml-[22%] mb-[10%] mt-[10%] "
+            />
+          </div>
+          <div>
+            <img
+              src={tail}
+              alt=""
+              className="w-[40%] sm:w-[70%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center "
+            />
+          </div>
+
+          <div>
+            <img
+              src={js}
+              alt=""
+              className="sm:w-[40%] w-[40%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[90%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+          <div>
+            <img
+              src={React}
+              alt=""
+              className="w-[50%] sm:h-[90%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+
+          <div>
+            <img
+              src={Nodejs}
+              alt=""
+              className="w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] sm:h-[90%]  hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110   text-align: center justify-content: center  align-items: center sm:ml-0"
+            />
+          </div>
+        </section>
+
+        <section className="mb-8 sm:ml-0 ml-[10%] sm:container text-center justify-center  sm:grid sm:grid-cols-6 sm:mx-auto sm:px-[5%] ">
+          <div>
+            <img
+              src={Express}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+
+          <div>
+            <img
+              src={MongoDb}
+              alt=""
+              className="sm:w-[49%]  w-[40%]  sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:m-0 ml-[22%] mb-[10%] mt-[10%] "
+            />
+          </div>
+          <div>
+            <img
+              src={Mongoose}
+              alt=""
+              className="w-[40%] sm:w-[70%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center "
+            />
+          </div>
+
+          <div>
+            <img
+              src={Git}
+              alt=""
+              className="sm:w-[40%] w-[40%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[90%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+          <div>
+            <img
+              src={Github}
+              alt=""
+              className="w-[50%] sm:h-[90%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+
+          <div>
+            <img
+              src={Netlify}
+              alt=""
+              className="w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] sm:h-[90%]  hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110   text-align: center justify-content: center  align-items: center sm:ml-0"
+            />
+          </div>
+        </section>
+
+        <section className="mb-8 sm:ml-0 ml-[10%] sm:container text-center justify-center  sm:grid sm:grid-cols-6 sm:mx-auto sm:px-[5%] ">
+          <div>
+            <img
+              src={boost}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+          <div>
+            <img
+              src={fire}
+              alt=""
+              className="w-[50%] sm:h-[90%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+          {/* <div>
+            <img
+              src={c}
+              alt=""
+              className="sm:w-[49%]  w-[40%]  sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:m-0 ml-[22%] mb-[10%] mt-[10%] "
+            />
+          </div> */}
+          <div>
+            <img
+              src={cc}
+              alt=""
+              className="w-[40%] sm:w-[70%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center "
+            />
+          </div>
+
+          <div>
+            <img
+              src={java}
+              alt=""
+              className="sm:w-[70%] w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[90%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+          <div>
+            <img
+              src={posman}
+              alt=""
+              className="w-[50%] sm:h-[90%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+
+          <div>
+            <img
+              src={redux}
+              alt=""
+              className="w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] sm:h-[90%]  hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110   text-align: center justify-content: center  align-items: center sm:ml-0"
+            />
+          </div>
+        </section>
+
+
+
+        <section className="mb-8 sm:ml-0 ml-[10%] sm:container text-center justify-center  sm:grid sm:grid-cols-6 sm:mx-auto sm:px-[5%] ">
+          <div>
+            <img
+              src={versal}
+              alt="html"
+              className="sm:w-[70%] w-[50%] sm:ml-0 ml-[20%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center "
+            />
+          </div>
+
+          <div>
+            <img
+              src={next}
+              alt=""
+              className="sm:w-[49%]  w-[40%]  sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:m-0 ml-[22%] mb-[10%] mt-[10%] "
+            />
+          </div>
+          <div>
+            <img
+              src={phpto}
+              alt=""
+              className="w-[40%] sm:w-[70%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center "
+            />
+          </div>
+
+          <div>
+            <img
+              src={illus}
+              alt=""
+              className="sm:w-[40%] w-[40%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[90%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+         
+
+          {/* <div>
+            <img
+              src={navtive}
+              alt=""
+              className="w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] sm:h-[90%]  hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110   text-align: center justify-content: center  align-items: center sm:ml-0"
+            />
+          </div> */}
+        </section>
+
+        {/* <section className="mb-8 mt-[10%]  container mx-auto sm:px-[5%] ">
+          <h2 className="text-2xl font-semibold mb-4 sm:m-0 mx-[5%]">Contact Form</h2>
+          <form className="p-4 bg-white shadow-md">
+            <label className="block mb-2">
+              <span className="text-gray-700">Your Message:</span>
+              <textarea
+                className="form-input outline-2 outline mt-1 block w-full"
+                rows="4"
+                placeholder="Type your message here..."
+              ></textarea>
+            </label>
+            <button className="bg-blue-500 text-white py-2 px-4 rounded">
+              Send Message
+            </button>
+          </form>
+        </section> */}
+      </main>
+      <div className="container mx-auto mt-[10%] text-center text-2xl">
+          <p>
+            <strong>Other Skills</strong>{" "}
+          </p>
+          <br />
+        </div>
+
+
+        <section className="mb-8 sm:ml-0 ml-[10%] sm:container text-center justify-center  sm:grid sm:grid-cols-6 sm:mx-auto sm:px-[5%] ">
+          <div>
+            <img
+              src={andhi}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+          <div>
+            <img
+              src={chatt}
+              alt=""
+              className="w-[50%] sm:h-[90%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+          {/* <div>
+            <img
+              src={c}
+              alt=""
+              className="sm:w-[49%]  w-[40%]  sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:m-0 ml-[22%] mb-[10%] mt-[10%] "
+            />
+          </div> */}
+          <div>
+            <img
+              src={gamma}
+              alt=""
+              className="w-[40%] sm:w-[70%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[95%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center "
+            />
+          </div>
+
+          <div>
+            <img
+              src={stack}
+              alt=""
+              className="sm:w-[70%] w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[15%] sm:h-[90%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+          <div>
+            <img
+              src={mic}
+              alt=""
+              className="w-[50%] sm:h-[90%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center sm:ml-0 "
+            />
+          </div>
+
+          <div>
+            <img
+              src={chat}
+              alt=""
+              className="w-[50%] sm:m-0 ml-[22%] mb-[10%] mt-[10%] sm:h-[90%]  hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110   text-align: center justify-content: center  align-items: center sm:ml-0"
+            />
+          </div>
+        </section>
+
+
+
+
+
+        <section className="mb-8 sm:ml-0 ml-[10%] sm:container text-center justify-center  sm:grid sm:grid-cols-6 sm:mx-auto sm:px-[5%] ">
+          <div>
+            <img
+              src={pp}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+         
+          <div>
+            <img
+              src={one}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+        
+          <div>
+            <img
+              src={two}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+          <div>
+            <img
+              src={three}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+          <div>
+            <img
+              src={four}
+              alt="html"
+              className="w-[70%] hover:drop-shadow-2xl hover:transition  hover:ease-in-out   hover:delay-150 hover:-translate-y-1 hover:scale-110  duration-300 text-align: center justify-content: center  align-items: center ml-[10%]"
+            />
+          </div>
+    
+        </section>
+      <footer className="bg-gray-800 text-white p-4 mt-8">
+        <p className="text-center">© 2024 Muhammad Abdullah Gohar</p>
+      </footer>
+    </>
+  );
+};
